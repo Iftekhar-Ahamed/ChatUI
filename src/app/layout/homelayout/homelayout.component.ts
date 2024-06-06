7374fda95bf890b0271bbb5286ae26cc8b007e6b
IFTEKHAR AHAMED SIDDIQUEE <iftekharahamed56@gmail.com>
2024-06-06 11:54:37 +0600

    homelayoutcomponent create

--- src/app/layout/homelayout/homelayout.component.ts
+++ src/app/layout/homelayout/homelayout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { ChatUserListComponent } from '../../shared/chat-user-list/chat-user-list.component';
+import { NavigationComponent } from '../../shared/navigation/navigation.component';
 
 @Component({
   selector: 'app-homelayout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ChatUserListComponent, NavigationComponent],
   templateUrl: './homelayout.component.html',
   styleUrl: './homelayout.component.css'
 })
---
+++ src/app/shared/chat-user-list/chat-user-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-user-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-user-list.component.html',
+  styleUrl: './chat-user-list.component.css'
+})
+export class ChatUserListComponent {
+
+}
