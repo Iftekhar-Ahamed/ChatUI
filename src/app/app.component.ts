c5fb45b6bc8150296c340f2c5a0ebbea4d2eccb8
IFTEKHAR AHAMED SIDDIQUEE <iftekharahamed56@gmail.com>
2024-06-10 18:02:16 +0600

    responsibe work flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ChatUI';
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId))
+      initFlowbite();
+  }
 }
