b77c3f0766a840fb1c19c673fcf63a809fd87dae
IFTEKHAR AHAMED SIDDIQUEE <iftekharahamed56@gmail.com>
2024-06-25 18:08:50 +0600

    extra code vs

--- src/app/shared/components/chat-ui/chat-ui.component.ts
+++ src/app/shared/components/chat-ui/chat-ui.component.ts
@@ -9,25 +9,30 @@ import { MessageCardComponent } from '../message-card/message-card.component';
 import { MessageInputComponent } from '../message-input/message-input.component';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
-import { ViewportScroller } from "@angular/common";
+import {ScrollingModule} from '@angular/cdk/scrolling';
 import { UserAction } from '../../../store';
+
+type Position = 'start' | 'mid' | 'end';
 
 @Component
 (
   {
     selector: 'app-chat-ui',
     standalone: true,
-    imports: [ MessageCardComponent, MessageInputComponent, CommonModule ],
+    imports: [ MessageCardComponent, MessageInputComponent, CommonModule,ScrollingModule ],
     templateUrl: './chat-ui.component.html',
     styleUrl: './chat-ui.component.css',
     providers: [],
   }
 )
+
 export class ChatUIComponent implements AfterViewChecked,OnInit,OnChanges
 {
-
+  
   @ViewChild('scrollframe') private scrollFrame!: ElementRef;
+  @ViewChild(CdkVirtualScrollViewport) viewPort: CdkVirtualScrollViewport;
   room$: Observable<Room | null>;
+  room : Room;
   isAlive: boolean = true;
   @Input() roomId! : string;
 
@@ -56,10 +61,27 @@ export class ChatUIComponent implements AfterViewChecked,OnInit,OnChanges
           {
             this.store.dispatch(new ItemLinkAction.UpdateUrl("home/chatList",this.router.url));
             this.store.dispatch(new UserAction.SelectUser(this.roomId));
+            if(x) this.room = x;
             return x;
           }
         )
       );
+  }
+
+  scroll(position: Position) {
+    let scrollIndex: number;
+    switch (position) {
+      case 'start':
+        scrollIndex = 0;
+        break;
+      case 'mid':
+        scrollIndex = this.room.messages.length / 2;
+        break;
+      case 'end':
+        scrollIndex = this.room.messages.length;
+        break;
+    }
+    this.viewPort.scrollToIndex(scrollIndex, 'smooth');
   }
 
   ngOnChanges(changes: SimpleChanges) {
