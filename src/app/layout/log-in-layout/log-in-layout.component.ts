3f13c310007ee6653baffa7bda51c9fd41d44bbb
IFTEKHAR <IFTEKHAR@DESKTOP-GRGSFNC>
2024-06-25 22:51:45 +0600

    log in init

--- src/app/layout/log-in-layout/log-in-layout.component.ts
+++ src/app/layout/log-in-layout/log-in-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-log-in-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './log-in-layout.component.html',
   styleUrl: './log-in-layout.component.css'
 })
