5d69a217d4d2a6aa2c9865a634419a588e52ce78
IFTEKHAR <IFTEKHAR@DESKTOP-GRGSFNC>
2024-06-26 23:28:02 +0600

    register added

--- src/app/shared/components/sign-up/sign-up.component.ts
+++ src/app/shared/components/sign-up/sign-up.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule,RouterLink],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit{
+  registerForm: FormGroup;
+    loading = false;
+    submitted = false;
 
+    constructor(
+        private formBuilder: FormBuilder
+    ) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        // this.authenticationService.register(this.registerForm.value)
+        //     .subscribe({
+        //         next: () => {
+        //             this.router.navigate(['/welcome/login']);
+        //         },
+        //         error: error => {
+        //             this.loading = false;
+        //         }
+        //     });
+    }
 }
