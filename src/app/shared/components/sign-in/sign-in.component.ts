8d2102d378b41fe912fdc33777b619d47c7b4a47
IFTEKHAR AHAMED SIDDIQUEE <iftekharahamed56@gmail.com>
2024-06-26 17:19:02 +0600

    sigin IN

--- src/app/shared/components/sign-in/sign-in.component.ts
+++ src/app/shared/components/sign-in/sign-in.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule,RouterLink],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit  {
 
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  
+
+  constructor( 
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ){
+    // if (this.authenticationService.currentUserValue) { 
+    //   this.router.navigate(['/']);
+    // }
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || 'signUpAccount';
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+        return;
+    }
+
+    this.loading = true;
+    // this.authenticationService.login(this.f.username.value, this.f.password.value)
+    //     .pipe(first())
+    //     .subscribe(
+    //         data => {
+    //             this.router.navigate([this.returnUrl]);
+    //         },
+    //         error => {
+    //             this.alertService.error(error);
+    //             this.loading = false;
+    //         });
+}
 }
