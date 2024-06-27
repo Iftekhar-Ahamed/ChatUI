a8e70c97773d3c508cf2de8bb49e106117842343
IFTEKHAR <IFTEKHAR@DESKTOP-GRGSFNC>
2024-06-27 23:03:04 +0600

    profile css

--- src/app/shared/components/profile/profile.component.ts
+++ src/app/shared/components/profile/profile.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { ItemLinkAction } from '../../../store/itemLink/itemLink.action';
+import { LogedInUserState } from '../../../store/logedInUser/logedInUser.state';
+import { LogedInUser } from '../../models/user.model';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  
+  @Select(LogedInUserState.user) user$!: Observable<LogedInUser>;
+
   constructor(
+    private store : Store
   ) {
+
   }
 }
