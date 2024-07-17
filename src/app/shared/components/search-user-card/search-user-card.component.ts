daa9d03b6c2a09621fada4758e6d8c282f808fe9
Iftekhar-Ahamed <iftekharahamed56@gmail.com>
2024-07-17 12:13:09 +0600

    init component "Search Bar, Search Result, Search User Card"

---
+++ src/app/shared/components/search-user-card/search-user-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-user-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-user-card.component.html',
+  styleUrl: './search-user-card.component.css'
+})
+export class SearchUserCardComponent {
+
+}
