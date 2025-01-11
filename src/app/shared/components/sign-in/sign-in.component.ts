8fa1aaad2039e97fd5d0c44114ec7c6504090324
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2025-01-11 22:06:56 +0600

    Sign-in done

--- src/app/shared/components/sign-in/sign-in.component.ts
+++ src/app/shared/components/sign-in/sign-in.component.ts
@@ -1,73 +1,98 @@
-import {CommonModule, NgOptimizedImage} from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Store} from "@ngxs/store";
+import {PasswordInputComponent} from "../password-input/password-input.component";
+import {TextInputComponent} from "../text-input/text-input.component";
+import {TextInputModel} from "../../models/common/ui-models";
+import {StringUtils} from "../../../utils/string.utils";
+import {lastValueFrom} from "rxjs";
 import {LogInRequestDto} from "../../models/user-log-in/user-log-in-request.model";
 import {UserInfoAction} from "../../../store/user-info/user-info.action";
 import {UserInfoState} from "../../../store/user-info/user-info.state";
 
 @Component({
-  selector: 'app-sign-in',
-  standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgOptimizedImage],
-  templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+    selector: 'app-sign-in',
+    standalone: true,
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, PasswordInputComponent, TextInputComponent],
+    templateUrl: './sign-in.component.html',
+    styleUrl: './sign-in.component.css'
 })
 
-export class SignInComponent implements OnInit
-{
-  loginForm: FormGroup;
-  returnUrl: string;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private store : Store,
-  ){}
-
-  ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      remember: [false]
-    });
-  }
-
-  get email() {
-    return this.loginForm.get('email');
-  }
-
-  get password() {
-    return this.loginForm.get('password');
-  }
-
-  async onSubmit() {
-
-    if (this.loginForm.invalid) {
-      this.loginForm.markAllAsTouched();
-      return;
-    }
-
-    const userLogInReq: LogInRequestDto = {
-      userName: this.loginForm.get('email')?.value,
-      password: this.loginForm.get('password')?.value
+export class SignInComponent {
+    usernameInput: TextInputModel = {
+        value: '',
+        errorMessage: '',
+        placeholder: 'Enter your registered email',
+        hasErrors: false
+    };
+    passwordInput: TextInputModel = {
+        value: '',
+        errorMessage: '',
+        placeholder: 'Enter your password',
+        hasErrors: false
     };
 
-    try {
-      const result = await this.store.dispatch(new UserInfoAction.userLogInAsync(userLogInReq)).toPromise();
-
-      this.store.select(UserInfoState.isUserLogIn).subscribe(isLoggedIn => {
-        if (isLoggedIn) {
-          const returnUrl = this.returnUrl || '/home';
-          this.router.navigate([returnUrl]);
-        }
-      });
-
-    } catch (error) {
-      console.error('An error occurred during login', error);
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private store: Store,
+    ) {
     }
-  }
 
+    validateInput(): boolean {
+
+        let isValid: boolean = true;
+        if (!StringUtils.isValidEmail(this.usernameInput.value)) {
+            this.usernameInput.hasErrors = true;
+            isValid = false;
+        } else {
+            this.usernameInput.hasErrors = false;
+        }
+
+        if (StringUtils.isEmptyOrWhitespace(this.passwordInput.value)) {
+            this.passwordInput.hasErrors = true;
+            isValid = false;
+        } else {
+            this.passwordInput.hasErrors = false;
+        }
+
+        return isValid;
+    }
+
+    async logIn() {
+
+        if(this.validateInput())
+        {
+            const userLogInReq: LogInRequestDto = {
+                userName: this.usernameInput.value,
+                password: this.passwordInput.value,
+            };
+
+            try {
+                const result = await lastValueFrom(this.store.dispatch(new UserInfoAction.userLogInAsync(userLogInReq)));
+
+                this.store.select(UserInfoState.isUserLogIn).subscribe(isLoggedIn => {
+                    if (isLoggedIn) {
+                        const returnUrl = '/home';
+                        this.router.navigate([returnUrl]);
+                    }
+                });
+
+            } catch (error) {
+                console.error('An error occurred during login', error);
+            }
+        }
+    }
+
+    signUp() {
+        this.router.navigate(['../sign-up'], {relativeTo: this.route}).then((r) => {
+            if (r) {
+                console.log('Navigation to /welcome/sign-up successful!');
+            } else {
+                console.log('Navigation to /welcome/sign-up failed!');
+            }
+        });
+    }
 }
