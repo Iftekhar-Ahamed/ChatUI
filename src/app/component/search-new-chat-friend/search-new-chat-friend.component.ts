feaf9bf47630359fc188258bb13f21dee675e664
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2025-01-17 10:24:59 +0600

    fix

--- src/app/component/search-new-chat-friend/search-new-chat-friend.component.ts
+++ src/app/component/search-new-chat-friend/search-new-chat-friend.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Store } from '@ngxs/store';
-import { CommonModule } from '@angular/common';
-import { SearchBarComponent } from '../../shared/components/search-bar/search-bar.component';
-import { ChatListAction } from '../../store';
-import { MenuNavigationAction } from '../../store/menu-navigation/menu-navigation.action';
-import {SearchResultComponent} from "../../shared/components/search-result/search-result.component";
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Store} from '@ngxs/store';
+import {CommonModule} from '@angular/common';
+import {ChatListAction} from '../../store';
+import {MenuNavigationAction} from '../../store/menu-navigation/menu-navigation.action';
+import {TextInputComponent} from "../../shared/components/text-input/text-input.component";
+import {ListViewComponent} from "../../shared/components/list-view/list-view.component";
+import {TextInputModel} from "../../shared/models/common/ui-models";
 
 @Component({
-  selector: 'app-search-new-chat-friend',
-  standalone: true,
-  imports: [SearchBarComponent,SearchResultComponent,CommonModule],
-  templateUrl: './search-new-chat-friend.component.html',
-  styleUrl: './search-new-chat-friend.component.css'
+    selector: 'app-search-new-chat-friend',
+    standalone: true,
+    imports: [CommonModule, TextInputComponent, ListViewComponent],
+    templateUrl: './search-new-chat-friend.component.html',
+    styleUrl: './search-new-chat-friend.component.css'
 })
-export class SearchNewChatFriendComponent implements OnInit
-{
-  constructor(private route:Router,private store:Store)
-  {}
+export class SearchNewChatFriendComponent implements OnInit {
+    searchTerm: TextInputModel = {
+        value: '',
+        errorMessage: '',
+        placeholder: 'Enter user email',
+        hasErrors: false
+    };
 
-  ngOnInit(): void {
-    this.store.dispatch( new ChatListAction.SelectNewChat());
-    this.store.dispatch(new MenuNavigationAction.UpdateMenuCurrentUrl("home/chatList",this.route.url));
-  }
+    constructor(private route: Router, private store: Store) {
+    }
+
+    ngOnInit(): void {
+        this.store.dispatch(new ChatListAction.SelectNewChat());
+        this.store.dispatch(new MenuNavigationAction.UpdateMenuCurrentUrl("home/chatList", this.route.url));
+    }
+
+    searchUserAsync() {
+
+    }
 }
