d868f784f73af11145384864c81da273cc485ecd
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2025-01-18 21:18:39 +0600

    fix error

--- src/app/shared/components/start-new-chat-card/start-new-chat-card.component.ts
+++ src/app/shared/components/start-new-chat-card/start-new-chat-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Store } from '@ngxs/store';
-import { NgClass, NgIf } from '@angular/common';
 import { ChatListState } from '../../../store';
 import { Observable, take} from 'rxjs';
 import { StartNewChatProperty } from '../../models/chatList.model';
